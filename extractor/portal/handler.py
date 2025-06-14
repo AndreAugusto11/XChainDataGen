857e7dcdbdb54fe18c2aadaccf1b2d05e9658da6
André Augusto <andre.augusto@tecnico.ulisboa.pt>
2025-06-14 16:59:29 +0100

    feat(portal-bridge): add support for Portal from Wormhole [WIP]
    Signed-off-by: André Augusto <andre.augusto@tecnico.ulisboa.pt>

--- extractor/portal/handler.py
+++ extractor/portal/handler.py
@@ -2,26 +2,24 @@ from typing import Any, Dict, List
 
 from config.constants import Bridge
 from extractor.base_handler import BaseHandler
-from extractor.cctp.constants import BRIDGE_CONFIG
-from extractor.cctp.utils.MessageBodyDecoder import MessageBodyDecoder
-from repository.cctp.repository import (
-    CCTPBlockchainTransactionRepository,
-    CCTPDepositForBurnRepository,
-    CCTPMessageReceivedRepository,
-)
+from extractor.portal.constants import BLOCKCHAIN_IDS, BRIDGE_CONFIG
+from extractor.portal.utils.PayloadDecoder import PayloadDecoder
 from repository.database import DBSession
+from repository.portal.repository import (
+    PortalBlockchainTransactionRepository,
+    PortalLogMessagePublishedRepository,
+    PortalTransferRedeemedRepository,
+)
 from utils.rpc_utils import RPCClient
 from utils.utils import CustomException, log_error, unpad_address
 
-from .constants import BLOCKCHAIN_IDS
 
+class PortalHandler(BaseHandler):
+    CLASS_NAME = "PortalHandler"
 
-class CctpHandler(BaseHandler):
-    CLASS_NAME = "CctpHandler"
-
-    def __init__(self, rpc_client: RPCClient, blockchains: list) -> None:
-        super().__init__(rpc_client, blockchains)
-        self.bridge = Bridge.CCTP
+    def __init__(self, rpc_client: RPCClient) -> None:
+        super().__init__(rpc_client)
+        self.bridge = Bridge.PORTAL
 
     def get_bridge_contracts_and_topics(self, bridge: str, blockchain: List[str]) -> None:
         self.get_bridge_contracts_and_topics(
@@ -31,9 +29,9 @@ class CctpHandler(BaseHandler):
         )
 
     def bind_db_to_repos(self):
-        self.blockchain_transaction_repo = CCTPBlockchainTransactionRepository(DBSession)
-        self.cctp_deposit_for_burn_repo = CCTPDepositForBurnRepository(DBSession)
-        self.cctp_message_received_repo = CCTPMessageReceivedRepository(DBSession)
+        self.blockchain_transaction_repo = PortalBlockchainTransactionRepository(DBSession)
+        self.log_message_published_repo = PortalLogMessagePublishedRepository(DBSession)
+        self.transfer_redeemed_repo = PortalTransferRedeemedRepository(DBSession)
 
     def handle_transactions(self, transactions: List[Dict[str, Any]]) -> None:
         func_name = "handle_transactions"
@@ -76,19 +74,18 @@ class CctpHandler(BaseHandler):
         events: List[Dict[str, Any]],
     ) -> List[Dict[str, Any]]:
         included_events = []
-
         for event in events:
             try:
                 if (
                     event["topic"]
-                    == "0x2fa9ca894982930190727e75500a97d8dc500233a5065e0f3126c48fbe0343c0"
-                ):  # DepositForBurn
-                    event = self.handle_deposit_for_burn(blockchain, event)
+                    == "0x6eb224fb001ed210e379b335e35efe88672a8ce935d981a6896b27ffdf52a3b2"
+                ):  # LogMessagePublished
+                    event = self.handle_log_message_published(blockchain, event)
                 elif (
                     event["topic"]
-                    == "0x58200b4c34ae05ee816d710053fff3fb75af4395915d3d2a771b24aa10e3cc5d"
-                ):  # MessageReceived
-                    event = self.handle_message_received(blockchain, event)
+                    == "0xcaf280c8cfeba144da67230d9b009c8f868a75bac9a528fa0474be1ba317c169"
+                ):  # TransferRedeemed
+                    event = self.handle_transfer_redeemed(blockchain, event)
 
                 if event:
                     included_events.append(event)
@@ -102,30 +99,35 @@ class CctpHandler(BaseHandler):
 
         return included_events
 
-    def handle_deposit_for_burn(self, blockchain, event):
-        func_name = "handle_deposit_for_burn"
+    def handle_log_message_published(self, blockchain, event):
+        func_name = "handle_log_message_published"
 
-        destination_chain = self.convert_doman_id_to_blockchain_name(event["destinationDomain"])
+        token_decimals = 18
 
-        if destination_chain is None:
+        print(len(event["payload"]))
+
+        if len(event["payload"]) != 266:
+            # this means that we are dealing with a transfer of another protocol on top of wormhole
             return None
 
         try:
-            if self.cctp_deposit_for_burn_repo.event_exists(
-                event["nonce"], blockchain, destination_chain
-            ):
+            payload_decoded = PayloadDecoder.decode(event["payload"], token_decimals)
+
+            dst_chain = self.convert_id_to_blockchain_name(payload_decoded["toChain"])
+
+            if dst_chain is None:
                 return None
 
-            self.cctp_deposit_for_burn_repo.create(
+            self.log_message_published_repo.create(
                 {
-                    "blockchain": blockchain,
-                    "transaction_hash": event["transaction_hash"],
+                    "amount": payload_decoded["originalAmount"],
+                    "token_address": unpad_address(payload_decoded["tokenAddress"]),
+                    "token_chain": payload_decoded["tokenChain"],
+                    "recipient": payload_decoded["to"],
+                    "recipient_chain": dst_chain,
+                    "fee": payload_decoded["fee"],
                     "nonce": event["nonce"],
-                    "depositor": event["depositor"].lower(),
-                    "burn_token": event["burnToken"].lower(),
-                    "recipient": unpad_address(event["mintRecipient"]),
-                    "dst_blockchain": destination_chain,
-                    "amount": event["amount"],
+                    "sequence_number": event["sequence"],
                 }
             )
             return event
@@ -136,40 +138,23 @@ class CctpHandler(BaseHandler):
                 f"{blockchain} -- Tx Hash: {event['transaction_hash']}. Error writing to DB: {e}",
             ) from e
 
-    def handle_message_received(self, blockchain, event):
-        func_name = "handle_message_received"
-
-        src_blockchain = self.convert_doman_id_to_blockchain_name(event["sourceDomain"])
-
-        if src_blockchain is None:
-            return None
-
-        if (
-            len(event["messageBody"]) != 264
-        ):  # there are other messages with different length that we don't want to process
-            return None
-
-        message_body = MessageBodyDecoder.decode(event["messageBody"])
+    def handle_transfer_redeemed(self, blockchain, event):
+        func_name = "handle_transfer_redeemed"
 
         try:
-            if self.cctp_message_received_repo.event_exists(
-                event["nonce"], src_blockchain, blockchain
-            ):
+            src_chain = self.convert_id_to_blockchain_name(event["emitter_chain_id"])
+
+            if src_chain is None:
                 return None
 
-            self.cctp_message_received_repo.create(
+            self.transfer_redeemed_repo.create(
                 {
-                    "blockchain": blockchain,
-                    "transaction_hash": event["transaction_hash"],
-                    "nonce": event["nonce"],
-                    "src_blockchain": src_blockchain,
-                    "input_token": message_body["input_token"],
-                    "depositor": message_body["depositor"],
-                    "recipient": message_body["recipient"],
-                    "amount": int(message_body["amount"], 16),
+                    "emitter_chain": src_chain,
+                    "emitter_address": unpad_address(event["emitter_address"]),
+                    "sequence_number": event["sequence"],
+                    "data": event["data"],
                 }
             )
-
             return event
         except Exception as e:
             raise CustomException(
@@ -178,17 +163,21 @@ class CctpHandler(BaseHandler):
                 f"{blockchain} -- Tx Hash: {event['transaction_hash']}. Error writing to DB: {e}",
             ) from e
 
-    def convert_doman_id_to_blockchain_name(self, id: str) -> str:
-        func_name = "convert_doman_id_to_blockchain_name"
+    def convert_id_to_blockchain_name(self, id: str) -> str:
+        func_name = "convert_id_to_blockchain_name"
 
         id = str(id)
 
+        # Discard smaller blockchains as we focus on the ones with more transferred value
+        # and only EVM-based blockchains. See list here https://wormhole.com/docs/build/reference/chain-ids/
+
         if id in BLOCKCHAIN_IDS:
             blockchain_name = BLOCKCHAIN_IDS[id]["name"]
-
+            # If the blockchain name is not in the list of blockchains specified by the user,
+            # return None
             if self.counterPartyBlockchainsMap.get(blockchain_name):
-                return blockchain_name
+                return BLOCKCHAIN_IDS[id]["name"]
         else:
-            CustomException(self.CLASS_NAME, func_name, f"Blockchain not found for Domain ID: {id}")
+            CustomException(self.CLASS_NAME, func_name, f"Blockchain not found for ID: {id}")
             # log_to_file(e, "data/out_of_scope_blockchains.log")
             return None
